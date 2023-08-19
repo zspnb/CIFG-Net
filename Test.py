d1312b9005cbdfd24259b79114a219dd315b695b
zsp <52599494+zspnb@users.noreply.github.com>
2023-08-19 15:30:21 +0800

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+import os, argparse
+from scipy import misc
+from lib1.a_c_d_b import PolypPVT
+from utils1.dataloader import test_dataset
+import cv2
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--testsize', type=int, default=352, help='testing size')
+    parser.add_argument('--pth_path', type=str, default='/media/1/hzp/Polyp-PVT/model_pth/FIN2_b5/PolypPVT/55PolypPVT.pth')
+    opt = parser.parse_args()
+    model = PolypPVT()
+    model.load_state_dict(torch.load(opt.pth_path))
+    model.cuda()
+    model.eval()
+    for _data_name in ['CVC-300', 'CVC-ClinicDB', 'Kvasir', 'CVC-ColonDB', 'ETIS-LaribPolypDB']:
+
+        ##### put data_path here #####
+        data_path = './dataset/TestDataset/{}'.format(_data_name)
+        ##### save_path #####
+        save_path = './result_map/PolypPVT/fin_b5_55/{}/'.format(_data_name)
+
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        image_root = '{}/images/'.format(data_path)
+        gt_root = '{}/masks/'.format(data_path)
+        num1 = len(os.listdir(gt_root))
+        test_loader = test_dataset(image_root, gt_root, 352)
+        for i in range(num1):
+            image, gt, name = test_loader.load_data()
+            gt = np.asarray(gt, np.float32)
+            gt /= (gt.max() + 1e-8)
+            image = image.cuda()
+            P1,P2,P3 = model(image)
+            res = F.upsample(P2, size=gt.shape, mode='bilinear', align_corners=False)
+            res = res.sigmoid().data.cpu().numpy().squeeze()
+            res = (res - res.min()) / (res.max() - res.min() + 1e-8)
+            cv2.imwrite(save_path+name, res*255)
+        print(_data_name, 'Finish!')
